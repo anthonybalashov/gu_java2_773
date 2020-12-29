f9a831924e94643968ded4badfeb64441dfb62d5
Anthony Balashov <anthony.balashov@gmail.com>
2020-12-29 16:16:27 +0300

    HW3

---
+++ src/ru/ipbalashov/ru/lesson3/hw_task1/task1.java
@@ -0,0 +1,26 @@
+package ru.ipbalashov.ru.lesson3.hw_task1;
+
+import java.util.*;
+
+public class task1 {
+    public static void main(String[] args) {
+        List<String> words = Arrays.asList(
+                "Mercedes", "BMW", "Audi", "Toyota", "Vokswagen",
+                "Opel", "Subaru", "Range Rover", "Toyota", "Vokswagen",
+                "BMW", "Audi", "Toyota", "Subaru", "Subaru",
+                "Audi", "Toyota", "Subaru", "Subaru", "Toyota"
+        );
+
+        Set<String> unique = new HashSet<String>(words);
+
+        System.out.println("Первоначальный массив");
+        System.out.println(words.toString());
+        System.out.println("Уникальные слова");
+        System.out.println(unique.toString());
+        System.out.println("Частота встречаемости слов");
+        for (String key : unique) {
+            System.out.println(key + ": " + Collections.frequency(words, key));
+        }
+    }
+
+}
