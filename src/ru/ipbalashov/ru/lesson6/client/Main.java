746026fcdeac28114e0390aa97ad4d841c88f372
Anthony Balashov <anthony.balashov@gmail.com>
2021-01-22 20:16:27 +0300

    HW 6

---
+++ src/ru/ipbalashov/ru/lesson6/client/Main.java
@@ -0,0 +1,7 @@
+package ru.ipbalashov.ru.lesson6.client;
+
+public class Main {
+    public static void main(String[] args) {
+        Client client = new Client();
+    }
+}
---
+++ src/ru/ipbalashov/ru/lesson6/server/Server.java
@@ -0,0 +1,34 @@
+package ru.ipbalashov.ru.lesson6.server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    public static void main(String[] args) {
+        ServerSocket server = null;
+        Socket socket = null;
+
+        //start server
+        try {
+            server = new ServerSocket(9000);
+            System.out.println("Server created. Waiting for client...");
+            //client connection monitoring
+            while (true) {
+                socket = server.accept();
+                System.out.println("Client connected");
+                new Thread(new ClientHandler(socket)).start();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                socket.close();
+                server.close();
+                System.out.println("Server closed");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
